b0409d3e34fc260a6b412c694ecb76eae47c77bc
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 15:50:55 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -281,4 +281,4 @@ function getCurrentPosition2() {
 }
 
 let currentLocation = document.querySelector("#current-location-button");
-currentLocation.addEventListener("click", getCurrentPosition);
+currentLocation.addEventListener("click", getCurrentPosition, getCurrentPosition2);
