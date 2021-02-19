b2adac4b7f77d20af40beea2207d7bb296866f8b
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 14:36:23 +0000

    CSS weather update

--- src/index.js
+++ src/index.js
@@ -257,7 +257,9 @@ function displayForecast(response){
           forecast.weather[0].icon
         }@2x.png"
         />
+        <div class="weather-forecast-temperature">
         <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+        </div>
      </div>`
  
 }
