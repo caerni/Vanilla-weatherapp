b5bff3689cb482e07daccee2efcfcc31451a4582
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 09:44:34 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -171,7 +171,12 @@ function searchWeather(city) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-function showForecast(location)
+function showForecast(location){
+  let apiKey = "1e7103a4d94dac75ab71913871657699";
+  let units = "metric";
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
   
 
 
