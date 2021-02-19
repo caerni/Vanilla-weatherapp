45bb67f554a13425ada86eb84e063af74d0babfc
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 16:03:26 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -266,11 +266,10 @@ function displayForecast(response){
 
 //5geoLocation button
 function showPositionForecast(position){
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
+  let city = position.coords.city;
   let units = "metric";
   let apiKey = "1e7103a4d94dac75ab71913871657699";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   
   axios.get(apiUrl).then(displayForecast);
  
