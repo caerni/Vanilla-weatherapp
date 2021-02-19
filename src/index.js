b415944dfb87b1144b032fde407790985b689758
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 14:30:12 +0000

    hour update

--- src/index.js
+++ src/index.js
@@ -248,7 +248,7 @@ function displayForecast(response){
   for (let index = 0; index < 6; index++) {
   forecast = response.data.list[index];
   forecastElement.innerHTML += ` 
-     <div class="col-2">
+     <div class="hourupdate">
        <h3>
          ${formatHours(forecast.dt * 1000)}
         </h3>
