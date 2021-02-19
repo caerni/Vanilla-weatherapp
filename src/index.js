f0a9edd4e69c10ef1d88816ab80ea9c5d192932d
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 11:38:57 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -250,10 +250,10 @@ function displayForecast(response){
      <h3>
      12.00
      </h3>
-     <img src = "http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
+     <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
       <br>
       <strong>${Math.round(forcast.main.temp_max)}°</strong> 
-      ${Math.round(forcast.main.temp_min)}
+      ${Math.round(forcast.main.temp_min)}°
     </div>
     </div>
   </div> 
