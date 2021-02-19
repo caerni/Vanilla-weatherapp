e6ff3accb8c23f6319f7083425a4e67f6f85b49d
caerni <65295460+caerni@users.noreply.github.com>
2021-02-19 12:37:41 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -131,21 +131,19 @@ small.innerHTML = `${time} ${day} ${month} ${date} ${year}`;
 
 
 
-
-function formatHours(timestamp){
-  let date = newdate(timestamp)
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10){
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 10){
-    minutes = `0${minutes}`
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  return  `${hours}:${minutes}`;
 
+  return `${hours}:${minutes}`;
 }
-
 
 
 
