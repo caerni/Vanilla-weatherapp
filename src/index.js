2d2b225524c4cb3a8f07b396eb36693f3311ddbe
caerni <65295460+caerni@users.noreply.github.com>
2021-02-20 10:37:14 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -195,19 +195,6 @@ function showPosition(position) {
 
 }
 //tempChange
-function showPositionForecast(position){
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiKey = "1e7103a4d94dac75ab71913871657699";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  
-  axios.get(apiUrl).then(displayForecast);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayForecast);
-
-}
 
 function showTemperature(response) {
   console.log(response.data);
@@ -288,10 +275,6 @@ function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-function getCurrentPosition2() {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition2(showPositionForecast);
-}
 
 let currentLocation = document.querySelector("#current-location-button");
 currentLocation.addEventListener("click", getCurrentPosition);
